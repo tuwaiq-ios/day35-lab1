4ba91f39c6ed235a1cf835f3d2a4377535e6785e
HANAN <hanan@HANANMacBook-Pro.local>
2021-11-11 20:08:09 +0300

    done✅

---
+++ TheMap1/TheMap1/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  TheMap1
+//
+//  Created by HANAN on 06/04/1443 AH.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    var locationManger: CLLocationManager!
+    
+    lazy  var mapView: MKMapView  = {
+        let mv = MKMapView()
+        mv.translatesAutoresizingMaskIntoConstraints = false
+        return mv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        mapView.delegate = self
+        
+        if (CLLocationManager.locationServicesEnabled())
+        {
+            locationManger = CLLocationManager()
+            locationManger.delegate = self
+            locationManger.desiredAccuracy = kCLLocationAccuracyBest
+            locationManger.requestAlwaysAuthorization()
+            locationManger.startUpdatingLocation()
+        }
+        
+        view .addSubview(mapView)
+        NSLayoutConstraint.activate([
+            mapView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            mapView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            mapView.topAnchor.constraint(equalTo: view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        
+        guard let location = locations.last else {return}
+        let lat = location.coordinate.latitude
+        let long = location.coordinate.longitude
+        
+        
+        let loc = MKPointAnnotation()
+        loc.title = "Asir"
+        loc.coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+        mapView.addAnnotation(loc)
+    }
+    
+}
+
+
+
